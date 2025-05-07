93a49dd2a74e15e02b55d27d5053e0076006d3ce
Filipe Rothe de Souza <filiperotherdesouza@Filipes-MacBook-Air.local>
2025-05-07 13:07:36 -0300

    fix-layout

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../../globals.css";
+
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "BioLink - Conectando a Produção de Biodiesel",
+  description: "Facilitando a Logística Reversa de Óleo Vegetal Usado"
+};
+
+export default function LoginLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html>
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
