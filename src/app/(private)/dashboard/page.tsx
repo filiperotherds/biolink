dcdd1c8928530837c3bd5672c7fb9588dcb3c2eb
Filipe <filiperotherds@gmail.com>
2025-05-26 14:26:40 -0300

    first endpoint categories

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,8 +1,22 @@
+'use client';
 
-export default function Dashboard() {
+import prisma from "@/lib/prisma";
+import { category } from "../../../../generated/prisma";
+
+export default async function Dashboard() {
+    const categories = await prisma.category.findMany()
+
+
     return (
         <main className="flex flex-col w-full h-full items-start justify-start">
-
+            <ul>
+                {categories.map((category: category) => (
+                    <li key={category.id} className="p-4 border-b">
+                        <h2 className="text-xl font-bold">{category.description}</h2>
+                        <p>{category.description}</p>
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
