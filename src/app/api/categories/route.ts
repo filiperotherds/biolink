4a22413929972584dfc91de55bd923b4b205cd17
Filipe <filiperotherds@gmail.com>
2025-05-26 14:44:40 -0300

    add users endpoint

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -2,6 +2,10 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function GET() {
-    const categories = await prisma.category.findMany();
-    return NextResponse.json(categories);
+    try {
+        const categories = await prisma.category.findMany();
+        return NextResponse.json(categories);
+    } catch (e) {
+        return NextResponse.json({ error: 'Failed to fetch users: ', e }, { status: 500 });
+    }
 }
