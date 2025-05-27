af19cacd736bc00a0e8a0a89149a969440627860
Filipe <filiperotherds@gmail.com>
2025-05-27 10:03:30 -0300

    change home layout dispose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,29 +10,29 @@ export default function Home() {
       <Header />
       <main className="min-h-screen flex flex-col items-center relative overflow-hidden gap-20">
         {/* Hero Section */}
-        <section className="w-full max-w-5xl h-screen mx-auto px-4 pt-36 text-center relative z-10">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight mb-8">
-            Gestão inteligente de óleo usado, sem esforço e sem custo.
-          </h1>
-          <p className="text-lg md:text-xl font-light text-zinc-400 max-w-2xl mx-auto mb-8">
-            Simplificamos o processo de coleta e reciclagem de óleo usado. Sua
-            empresa em dia, sem complicações e sem custos.
-          </p>
-          <Link
-            href="/signup"
-            className="inline-flex h-[38px] items-center justify-center rounded-md bg-primary px-4 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90"
-          >
-            Cadastre-se
-          </Link>
-
-          <div className="absolute bottom-20">
-            {/* Partners Carousel */}
-            <PartnersCarousel />
+        <section className="w-full max-w-5xl h-[calc(100vh-64px)] flex flex-col justify-around mx-auto px-4 pt-36 text-center relative z-10">
+          <div>
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight mb-12">
+              Gestão inteligente de óleo usado, sem esforço e sem custo.
+            </h1>
+            <p className="text-lg md:text-xl font-light text-zinc-400 max-w-2xl mx-auto mb-12">
+              Simplificamos o processo de coleta e reciclagem de óleo usado. Sua
+              empresa em dia, sem complicações e sem custos.
+            </p>
+            <Link
+              href="/signup"
+              className="inline-flex h-[38px] items-center justify-center rounded-md bg-primary px-4 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90"
+            >
+              Cadastre-se
+            </Link>
           </div>
+
+          {/* Partners Carousel */}
+          <PartnersCarousel />
         </section>
 
         {/* Feature Section */}
-        <FeatureSection/>
+        <FeatureSection />
       </main>
       <Footer />
     </>
